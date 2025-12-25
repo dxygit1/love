7be94eb6be52ab68b5d0633243789272417046ae
tianxing <960897265@qq.com>
2025-12-25 14:57:28 +0800

    feat: add microsoft clarity tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,15 @@ export default function RootLayout({
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "uqx8xto7d1");
+          `}
+        </Script>
         <LanguageProvider>
           <Header />
           {children}
